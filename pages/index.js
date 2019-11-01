2e8c9f534b4b16aff588bbd31b182075d5783e50
Goce Cvetanov <cvetanovgoce@gmail.com>
2019-11-01 08:39:44 +0100

    Add comment from docs about getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ const Index = (props) => (
   </Layout>
 );
 
+// Make sure the returned object from `getInitialProps` is a plain `Object` and not using `Date`, `Map` or `Set`
 Index.getInitialProps = async function() {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
   const data = await res.json();
